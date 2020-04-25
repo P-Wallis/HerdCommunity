cea80a02ec715beaa43385592a0b7b3aecebc7a6
Sean Craig <seancraig21@gmail.com>
2020-04-25 17:56:34 -0500

    started adding getClosestEnemy

--- Assets/Scripts/Predator.cs
+++ Assets/Scripts/Predator.cs
@@ -6,16 +6,40 @@ public class Predator : MonoBehaviour
 {
 	public Transform follow;
 	public Flock flock;
+    private bool target;
+
     // Start by finding the camera to follow and copy its position
     void Start()
     {
-        
+        target = false;
     }
 
     // Update its position based on the movement of the flock
     void Update()
     {
-        transform.position = follow.position + (Vector3.right * 3);
-        //flock.boids
+        transform.position = follow.position + (Vector3.right * 6);
+       /* if (target = false) {
+           transform. GetClosestEnemy(flock.boids);
+        }*/
     }
+
+    /*Transform GetClosestEnemy (Transform[] enemies)
+    {
+        Transform bestTarget = null;
+        float closestDistanceSqr = Mathf.Infinity;
+        Vector3 currentPosition = transform.position;
+        foreach(Transform potentialTarget in enemies)
+        {
+            Vector3 directionToTarget = potentialTarget.position - currentPosition;
+            float dSqrToTarget = directionToTarget.sqrMagnitude;
+            if(dSqrToTarget < closestDistanceSqr)
+            {
+                closestDistanceSqr = dSqrToTarget;
+                bestTarget = potentialTarget;
+            }
+        }
+     
+        return bestTarget;
+    }*/
+
 }
